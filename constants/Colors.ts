84f0b29ebac2d2205cff61e2457bb3a8d62d2466
Gonzalo <gonalc8@gmail.com>
2025-08-31 14:46:47 +0200

    remove unused colors

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,11 +1,3 @@
-/**
- * Below are the colors that are used in the app. The colors are defined in the light and dark mode.
- * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
- */
-
-const tintColorLight = '#0a7ea4';
-const tintColorDark = '#fff';
-
 export const Colors = {
   light: {
     /**
@@ -20,10 +12,6 @@ export const Colors = {
      * @default Backgrounds, cards and large UI surfaces.
      */
     background: '#F8F9FA',
-    tint: tintColorLight,
-    icon: '#687076',
-    tabIconDefault: '#687076',
-    tabIconSelected: tintColorLight,
     /**
      * Electric Blue
      *
@@ -68,10 +56,6 @@ export const Colors = {
      * @default Backgrounds, cards and large UI surfaces.
      */
     background: '#1C2526',
-    tint: tintColorDark,
-    icon: '#9BA1A6',
-    tabIconDefault: '#9BA1A6',
-    tabIconSelected: tintColorDark,
     /**
      * Neon Blue
      *
