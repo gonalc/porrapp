26bc95767d442581e3d91b7bb5942111689fb517
Gonzalo <gonalc8@gmail.com>
2025-09-18 15:52:51 -0600

    style shareable code

--- components/polls/SharePollModal.tsx
+++ components/polls/SharePollModal.tsx
@@ -1,20 +1,64 @@
 import { Modal } from "@/components/Modal";
 import { ThemedText } from "@/components/ThemedText";
 import { type Poll } from "@/hooks/supabase/polls/getPolls";
+import { useThemeColor } from "@/hooks/useThemeColor";
+import { StyleSheet, TouchableOpacity } from "react-native";
 
 type SharePollModalProps = {
   visible: boolean;
   onClose: () => void;
   poll: Poll | null;
-}
+};
 
-export function SharePollModal({ visible, onClose, poll }: SharePollModalProps) {
+export function SharePollModal({
+  visible,
+  onClose,
+  poll,
+}: SharePollModalProps) {
+  const backgroundColor = useThemeColor({}, "background");
+  const neutralSupportColor = useThemeColor({}, "neutralSupport");
+
   if (!poll) return null;
 
   return (
     <Modal visible={visible} onRequestClose={onClose}>
-      <ThemedText type="defaultSemiBold">Sharing Poll!</ThemedText>
-      <ThemedText>{poll.code}</ThemedText>
+      <ThemedText type="defaultSemiBold">Comparte esta porra</ThemedText>
+      <ThemedText>
+        Utiliza este código para que más personas se unan a la porra.
+      </ThemedText>
+
+      <TouchableOpacity onPress={() => alert(poll.code)}>
+        <ThemedText
+          type="subtitle"
+          style={[
+            styles.code,
+            {
+              borderColor: neutralSupportColor,
+              backgroundColor: backgroundColor,
+            },
+          ]}
+        >
+          {poll.code}
+        </ThemedText>
+      </TouchableOpacity>
+
+      <ThemedText style={styles.caption}>
+        Toca el código para copiarlo
+      </ThemedText>
     </Modal>
-  )
+  );
 }
+
+const styles = StyleSheet.create({
+  code: {
+    borderWidth: 1,
+    borderRadius: 8,
+    marginHorizontal: "auto",
+    marginTop: 16,
+    paddingHorizontal: 16,
+    paddingVertical: 8,
+  },
+  caption: {
+    textAlign: "center",
+  },
+});
