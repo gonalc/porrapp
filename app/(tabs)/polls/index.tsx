e053fddd97f04968483a6837070c0e9b4d2aa84a
Gonzalo <gonalc8@gmail.com>
2025-09-22 08:31:14 -0600

    show unlogged screen in polls

--- app/(tabs)/polls/index.tsx
+++ app/(tabs)/polls/index.tsx
@@ -1,9 +1,22 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
 
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
+import { useSession } from "@/contexts/session";
 
 export default function PollsScreen() {
+  const { data: session } = useSession();
+
+  if (!session) {
+    return (
+      <ThemedView style={[styles.container, styles.loggedOut]}>
+        <ThemedText type="defaultSemiBold" style={styles.loggedOutMessage}>
+          Para poder crear o unirte a una porra, debes iniciar sesión.
+        </ThemedText>
+      </ThemedView>
+    );
+  }
+
   return (
     <ThemedView style={styles.container}>
       <ThemedText type="title">Nuevas Porras!</ThemedText>
@@ -15,6 +28,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingHorizontal: 32,
-    paddingTop: 64
+    paddingTop: 64,
   },
+  loggedOut: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  loggedOutMessage: {
+    fontSize: 20,
+    textAlign: "center",
+  }
 });
